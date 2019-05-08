092f1dd230eb61c80faa71d1818bcbc88215bab7
Ben Chaimo <doroza@hotmail.com>
2019-05-08 20:09:57 +0300

    responsive component and bgimage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
-import { MatSidenavModule, MatButtonModule, MatToolbarModule, MatIconModule, MatListModule } from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatSidenavModule, MatButtonModule,
+  MatToolbarModule, MatIconModule, MatListModule,
+  MatCardModule
+} from '@angular/material';
+
 import { HomeComponent } from './home/home.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
 import { CustomerPageComponent } from './customer-page/customer-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +25,8 @@ import { ContactPageComponent } from './contact-page/contact-page.component';
     HomeComponent,
     MainNavComponent,
     CustomerPageComponent,
-    ContactPageComponent
+    ContactPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -32,6 +39,7 @@ import { ContactPageComponent } from './contact-page/contact-page.component';
     MatToolbarModule,
     MatIconModule,
     MatListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
